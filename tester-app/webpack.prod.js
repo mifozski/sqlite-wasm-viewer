08b7067ed2b92e600cbbfc7e2832fca447fd090e
mifozski@gmail.com <mifozski@gmail.com>
2023-11-09 21:04:30 -0300

    build tester app for live demo

---
+++ tester-app/webpack.prod.js
@@ -0,0 +1,70 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = [
+    {
+        target: 'web',
+        entry: './src/index.ts',
+        output: {
+            path: path.join(__dirname, 'dist'),
+            filename: 'index.js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|js)x?$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                        presets: ['@babel/env', '@babel/typescript'],
+                    },
+                },
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader'],
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.ts', '.js'],
+            plugins: [
+                new TsconfigPathsPlugin({
+                    configFile: './tsconfig.json',
+                }),
+            ],
+        },
+        mode: 'development',
+        node: {
+            __dirname: false,
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+            }),
+        ],
+        devServer: {
+            static: {
+                directory: path.join(__dirname, 'public'),
+            },
+            compress: true,
+            port: 9000,
+            historyApiFallback: {
+                index: './src/index.html',
+            },
+            headers: [
+                {
+                    key: 'Cross-Origin-Embedder-Policy',
+                    value: 'require-corp',
+                },
+                {
+                    key: 'Cross-Origin-Opener-Policy',
+                    value: 'same-origin',
+                },
+            ],
+        },
+    },
+];
